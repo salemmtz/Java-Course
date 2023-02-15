ea0795c903ed2880172d48a068b78a8eca5cd156
salemmtz <angeldesalem@gmail.com>
2023-02-14 18:29:29 -0600

    Tue 14 end of day windows computer

--- OOP_Part1/src/Main.java
+++ OOP_Part1/src/Main.java
@@ -80,16 +80,50 @@ public class Main {
 //        }
 
         // THE RECORD
-        for (int i = 1; i<=5; i++){
+       /* for (int i = 1; i<=5; i++){
             LPAStudent s = new LPAStudent("S92300" + i, switch(i){
                 case 1 -> "Mary";
                 case 2 -> "Carol";
-                case 3 -> "Tim";  
+                case 3 -> "Tim";
                 case 4 -> "Harry";
                 case 5 -> "Lisa";
                 default -> "Anonymous";
             }, "05/11/1985", "Java Masterclass");
             System.out.println(s);
-        }
+        }*/
+
+
+
+        ////////////////////////////////////////////////////////////////////////////////////////
+        ////////////  INHERITANCE  ///////////
+        ///////////////////////////////////////////////////////////////////////////////////////
+
+        Animal animal = new Animal("Generic Animal", "Huge", 400);
+        doAnimalStuff(animal,"slow");
+
+        Dog dog = new Dog();
+        doAnimalStuff(dog,"  ");
+
+        Dog yorkie = new Dog("Yorkie",15);
+        doAnimalStuff(yorkie, "fast");
+
+        Dog retriever = new Dog("Labrador Retriever", 65, "Floppy","Swimmer");
+        doAnimalStuff(retriever,"slow");
+
+        Dog wolf = new Dog("Wolf", 40);
+        doAnimalStuff(wolf,"slow");
+
+        Fish goldie = new Fish("Goldfish", 0.25,2,3);
+        doAnimalStuff(goldie, "fast");
+
+    }
+
+    public static void doAnimalStuff(Animal animal,String speed){
+        animal.makesNoise();
+        animal.move(speed);
+        System.out.println(animal);
+        System.out.println("_ _ _ _");
+
+
     }
 }
